c1d096087610bbff0d9f958f35c8aabb51852ea4
unknown <Administrator@wscsc308.iemop.ph>
2023-11-24 10:39:21 +0800

    Fixing Cash Collection checkbox

--- AccountsManagement/AccountsManagementForms/MainForm.Designer.vb
+++ AccountsManagement/AccountsManagementForms/MainForm.Designer.vb
@@ -927,7 +927,7 @@ Partial Class MainForm
         Me.MainMenuStrip = Me.MenuStrip
         Me.Name = "MainForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Accounts Management System (LUZVIS v4.1.997c1d9) "
+        Me.Text = "Accounts Management System (LVM v4.1.6efa41c) "
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.MenuStrip.ResumeLayout(False)
         Me.MenuStrip.PerformLayout()
